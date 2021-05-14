50943677f74230cff3905f6a72e14845f9161300
Magnus Rosén <mangez84@gmail.com>
2021-05-14 14:54:50 +0000

    Add functions to be able to send feedback via the form

---
+++ assets/js/enjoytrivia-feedback.js
@@ -0,0 +1,18 @@
+/*
+Function and jQuery event listener for sending feedback to the developer.
+Parts of the code below was copied from https://www.emailjs.com/docs/sdk/send-form/
+*/
+
+$("#form-feedback").submit(function (event) {
+  event.preventDefault();
+  sendFeedback();
+});
+
+function sendFeedback() {
+  emailjs.sendForm('default_service', 'enjoytrivia', '#form-feedback')
+    .then(function (response) {
+      console.log('SUCCESS!', response.status, response.text);
+    }, function (error) {
+      console.log('FAILED...', error);
+    });
+}
